c4a30e44c66b18cd77ebff584acf3dc13d874d42
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-22 10:01:22 +0300

    refactored task.ps1 script

--- task.ps1
+++ task.ps1
@@ -2,4 +2,9 @@
 #Remove-AzVMDataDisk -VM $VirtualMachine -Name "mate-disk-task-3"
 #Update-AzVM -ResourceGroupName "mate-azure-task-5" -VM $VirtualMachine
 
-$Disks = Get-AzDisk -ResourceGroupName "mate-azure-task-5" | Where-Object {$_.DiskState -eq "Unattached"} | ConvertTo-Json | Out-File -Path "result.json"
+Write-Host "Checking 'mate-azure-task-5' resource group for unttached disks..."
+$Disks = Get-AzDisk -ResourceGroupName "mate-azure-task-5" | Where-Object {$_.DiskState -eq "Unattached" -and $_.OsType -eq $null -and $_.ManagedBy -eq $null}
+
+Write-Host ("Finded {0} disks" -f $Disks.Count)
+Write-Host "Write result disks to 'result.json'..."
+@($Disks) | ConvertTo-Json | Out-File -Path "result.json"
