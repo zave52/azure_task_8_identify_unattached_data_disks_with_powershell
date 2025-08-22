c74e1aa9fc80264bb0dd56c45e9a8f6a7d63b8af
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-22 09:33:52 +0300

    added task.ps1 script for task

--- task.ps1
+++ task.ps1
@@ -1,1 +1,5 @@
-# Write your code here
+#$VirtualMachine = Get-AzVM -ResourceGroupName "mate-azure-task-5" -Name "mate-vm-task-2"
+#Remove-AzVMDataDisk -VM $VirtualMachine -Name "mate-disk-task-3"
+#Update-AzVM -ResourceGroupName "mate-azure-task-5" -VM $VirtualMachine
+
+$Disks = Get-AzDisk -ResourceGroupName "mate-azure-task-5" | Where-Object {$_.DiskState -eq "Unattached"} | ConvertTo-Json | Out-File -Path "result.json"
